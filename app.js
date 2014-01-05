253252d8a11df1e12d23f44bdced57750be68187
Irfan Nurhakim <myfunareaa@gmail.com>
2014-01-05 21:28:58 +0700

    init

---
+++ app.js
@@ -0,0 +1,28 @@
+var twitter = require('ntwitter');
+var util = require('util');
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+var db = new Db('twitter', new Server('localhost', 27017, {safe: false}, {auto_reconnect: true}, {}));
+var ctr = 1;
+
+var twit = new twitter({
+  consumer_key: 'aTIpG0jGl6nwp4KWhUILBQ',
+  consumer_secret: 'cUmQNsQ6u4wn4PUcoQ9PmqzFEzVmzJmGnD2gROeY',
+  access_token_key: '44984017-EKeo9ivt957w7Y6uiT2NsCFbM1PQ46KF0c5J41egQ',
+  access_token_secret:'DvTKLNq0Nev4XriCRm8cdfRkz7uYtbgddjcl0amOSis'
+});
+
+
+db.open(function(err, db){
+	var collection = db.collection('raw_data');
+
+	twit.stream('statuses/filter', {'track': 'happy new year, 2014, new year, newyear'}, function(stream){
+		stream.on('data', function(data){
+			//console.log(ctr++, new Date());
+			collection.insert(data);
+			//console.log(data.text);
+		});	
+	});
+});
+
